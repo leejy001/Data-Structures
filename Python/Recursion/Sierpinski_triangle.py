c04687ca3b52dd11632b8e2330562df8b62bb4f2
leejy001 <greatness007@naver.com>
2021-04-02 17:04:42 +0900

    시에르핀스키 삼각형

---
+++ Python/Recursion/Sierpinski_triangle.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+def drawTriangle(x, y, size):
+    if size >= 30:
+        drawTriangle(x, y, size/2)                              # 왼쪽 아래 작은 삼각형
+        drawTriangle(x+size/2, y, size/2)                       # 오른쪽 아래 작은 삼각형
+        drawTriangle(x+size/4, int(y-size*(3**0.5)/4), size/2)  # 위쪽 작은 삼각형
+    else:
+        canvas.create_polygon(x, y, x+size, y, x+size/2, y-size*(3**0.5)/2, fill='red', outline="red")
+
+wSize = 1000
+radius = 400
+
+window = Tk()
+window.title("삼각형 모양의 프랙탈")
+canvas = Canvas(window, height=wSize, width=wSize, bg='white')
+
+drawTriangle(wSize/5, wSize/5*4, wSize*2/3)
+
+canvas.pack()
+window.mainloop()
